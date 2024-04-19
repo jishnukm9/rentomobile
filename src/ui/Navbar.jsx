e01a4e33973ce78da1f6f00df24b011cfb5b8676
jishnukm <jishnukm2010@gmail.com>
2024-04-19 10:13:49 +0530

    modified ui

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Logo from './Logo'
+import { HiUserCircle } from "react-icons/hi";
 
 export default function Navbar() {
   return (
     <div>
     <div className="max-w-[1200px] px-10 py-8 mx-auto my-0 flex justify-between">
         <Logo/>
-        <p  className='text-white'>Logout</p>
+        <p  className='text-white'>
+          <HiUserCircle className='text-3xl' />
+        </p>
     </div>
     </div>
   )
