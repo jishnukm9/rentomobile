006d21dfe106e0afe8ac670beb3f3d45f0a20f8e
jishnukm9 <jishnukm2010@gmail.com>
2026-04-03 10:12:59 +0530

    ui change

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,15 +1,15 @@
-import React, { useContext } from 'react'
-import Logo from './Logo'
+import React, { useContext } from "react";
+import Logo from "./Logo";
 import { HiUserCircle } from "react-icons/hi";
-import { SearchContext } from '../context/SearchContext';
+import { SearchContext } from "../context/SearchContext";
 
 export default function Navbar() {
   const { cities, selectedCity, handleCityChange } = useContext(SearchContext);
 
   return (
     <div>
-    <div className="max-w-[1200px] px-10 py-8 mx-auto my-0 flex justify-between items-center">
-        <Logo/>
+      <div className="max-w-[1200px] px-10 py-8 mx-auto my-0 flex justify-between items-center">
+        <Logo />
         <div className="flex items-center gap-4">
           <select
             value={selectedCity}
@@ -17,16 +17,20 @@ export default function Navbar() {
             className="bg-white/20 backdrop-blur-sm text-white border border-white/30 rounded-md px-4 py-2 font-semibold cursor-pointer focus:outline-none focus:ring-2 focus:ring-yellow-400"
           >
             {cities.map((city) => (
-              <option key={city.id} value={city.id} className="text-black bg-white">
+              <option
+                key={city.id}
+                value={city.id}
+                className="text-black bg-white"
+              >
                 {city.name}
               </option>
             ))}
           </select>
-          <p className='text-white'>
+          {/* <p className='text-white'>
             <HiUserCircle className='text-3xl' />
-          </p>
+          </p> */}
         </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
